821a7bd0633491d0aec59e19acf17e853e196a30
Szymon Koper <sakydpozrux@gmail.com>
2014-06-01 02:49:42 +0200

    Quick working example. (has strange bug with showing second to last insead of last read line)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,85 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow), serial()
 {
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    closeSerialPort();
     delete ui;
+}
+
+void MainWindow::openSerialPort()
+{
+    QString name = "/dev/ttyACM0";
+    QString stringBaudRate = "9600";
+    QString stringDataBits = "8bits";
+
+    serial.setPortName(name);
+
+    if (serial.open(QIODevice::ReadWrite))
+    {
+        serial.setBaudRate(QSerialPort::Baud9600);
+        serial.setDataBits(QSerialPort::Data8);
+        serial.setParity(QSerialPort::NoParity);
+        serial.setStopBits(QSerialPort::OneStop);
+        serial.setFlowControl(QSerialPort::NoFlowControl);
+
+        ui->statusBar->showMessage(
+                    QString("Connected to %1 : %2, %3, %4, %5, %6. Why not try sending dupa???")
+                    .arg(name)
+                    .arg(stringBaudRate)
+                    .arg(stringDataBits));
+    }
+    else
+    {
+        QMessageBox::critical(this, "Error", serial.errorString());
+        ui->statusBar->showMessage("OMG open error!!!! WTF IS HAPPENING!!!");
+    }
+}
+
+void MainWindow::closeSerialPort()
+{
+    serial.close();
+    ui->statusBar->showMessage("You closed connection... WHYYYYYY");
+}
+
+void MainWindow::readData()
+{
+    QByteArray data = serial.readAll();
+    ui->plainTextEdit_2->appendPlainText(QString(data));
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString line_to_send = ui->lineEdit->text();
+    if (line_to_send == "dupa") line_to_send = "sam jestes glupi";
+
+    QByteArray data = QByteArray((line_to_send + '\n').toStdString().c_str());
+    serial.write(data);
+    readData();
+
+
+    ui->plainTextEdit->appendPlainText(line_to_send);
+}
+
+void MainWindow::on_checkBox_toggled(bool checked)
+{
+    if (checked)
+    {
+        openSerialPort();
+        ui->pushButton->setEnabled(true);
+    }
+    else
+    {
+        closeSerialPort();
+        ui->pushButton->setEnabled(false);
+    }
 }
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -2,6 +2,8 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QtCore>
 
 namespace Ui {
 class MainWindow;
@@ -10,13 +12,24 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private:
+    Ui::MainWindow *ui;
+    QSerialPort serial;
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    void openSerialPort();
+    void closeSerialPort();
+
+    void readData();
+
+private slots:
+    void on_pushButton_clicked();
+
+
+    void on_checkBox_toggled(bool checked);
 };
 
 #endif // MAINWINDOW_HPP
